4e11e3d4097dfafc25e4a1b39929246fe34c86c7
amrgharz <ammor.ghr@gmail.com>
2020-02-04 12:58:04 +0400

    message

--- src/store/reduceres/ProjectReducer.js
+++ src/store/reduceres/ProjectReducer.js
@@ -1,4 +1,10 @@
-const initState = {};
+const initState = {
+    projects: [
+        {id:'1', title:'first project', content:'I am the first project'},
+        {id:'2', title:'second project', content:'I am the second project'},
+        {id:'3', title:'third project', content:'I am the third project'}
+]
+};
 
 const ProjectReducer = (state= initState, action) =>{
     return state
